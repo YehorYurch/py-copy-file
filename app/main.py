838d57ed1e88b6e306c854c114064c98a1195a7e
Yehor Yurchenko <yegoryurtchenko05@gmail.com>
2025-03-11 12:31:32 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-def copy_file(command) -> None:
+def copy_file(command: str) -> str:
     split_command = command.split()
-    if len(split_command) != 3 or split_command[1] == split_command[2] or "cp" not in split_command:
-        return
+    if len(split_command) != 3 or split_command[1] == split_command[2] or split_command[0] != "cp":
+        return "Error, information entered incorrectly"
     try:
         with open(split_command[1], "r") as f1, open(split_command[2], "w") as f2:
             info_to_copy = f1.read()
